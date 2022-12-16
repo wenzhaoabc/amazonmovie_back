4fb163ece2d36001be4f95041e599409872fcd9c
wenzhaoabc <wenzhaoabc@gmail.com>
2022-12-17 00:23:56 +0800

    创建DTO

---
+++ src/main/java/com/tongji/controller/HiveController.java
@@ -0,0 +1,15 @@
+package com.tongji.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hive相关的请求API
+ *
+ * @author 赵帅涛
+ * @since 2022年12月16日
+ */
+@RestController
+@RequestMapping("/hive")
+public class HiveController {
+}
