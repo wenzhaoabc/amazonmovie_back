2448ce4e3d393a7dd44f4c72bf6645aeaadb3a92
cuizhenhao <1278493070@qq.com>
2022-12-16 13:18:12 +0800

    溯源查询

--- src/main/java/com/tongji/controller/MovieController.java
+++ src/main/java/com/tongji/controller/MovieController.java
@@ -1,13 +1,19 @@
 package com.tongji.controller;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.tongji.entity.Movie;
+import com.tongji.entity.Origin;
 import com.tongji.service.HMovieService;
 import com.tongji.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
 @RestController
-@RequestMapping("/movie")
+@RequestMapping("/api/movie")
 public class MovieController {
     @Autowired
     private MovieService movieService;
@@ -25,5 +31,17 @@ public class MovieController {
         Movie m = hMovieService.getById(id);
         System.out.println(m.toString());
         return m;
+    }
+
+    @CrossOrigin
+    @RequestMapping(value = "/mysql/getOrigin", method = RequestMethod.GET)
+    public Object getOrigin(HttpServletRequest request) {
+        String title = request.getParameter("searchingTitle").toString();
+        List<Origin> movies = movieService.getOrigin(title);
+        JSONObject jsonObject = new JSONObject();
+        JSONArray jsonArray = new JSONArray();
+        jsonArray.addAll(movies);
+        jsonObject.put("movies", jsonArray);
+        return jsonObject;
     }
 }
